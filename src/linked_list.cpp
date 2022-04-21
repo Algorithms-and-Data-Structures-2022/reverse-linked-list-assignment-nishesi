1cd83cbc970e437d5a554bfb2850b9b189e85bf3
Nurislam Zaripov <znurislama56@gmail.com>
2022-04-21 17:24:41 +0300

    done

--- src/linked_list.cpp
+++ src/linked_list.cpp
@@ -13,6 +13,30 @@ namespace assignment {
    */
   void LinkedList::ReverseIterative() {
     // Напишите здесь свой код ...
+    if (size_ < 2) {
+      return;
+    }
+    if (size_ == 2) {
+      back_->next = front_;
+      front_->next = nullptr;
+      front_ = back_;
+      back_ = front_->next;
+      return;
+    }
+    Node* curr = front_;
+    Node* currN = curr->next;
+    Node* currNN = currN->next;
+
+    for (int i = 0; i < size_ - 2; ++i) {
+      currN->next = curr;
+      curr = currN;
+      currN = currNN;
+      currNN = currNN->next;
+    }
+    currN->next = curr;
+    back_ = front_;
+    back_->next = nullptr;
+    front_ = currN;
   }
 
   /**
@@ -24,11 +48,25 @@ namespace assignment {
    */
   void LinkedList::ReverseRecursive() {
     // Напишите здесь свой код ...
+    if (front_ != nullptr) {
+      Node* curr_node = front_;
+      Node* prev_node = nullptr;
+      reverse_recursive_helper(curr_node, prev_node);
+    }
   }
 
   // вспомогательный метод для реализации рекурсии
   void LinkedList::reverse_recursive_helper(Node*& curr, Node* prev) {
     // Напишите здесь свой код ...
+    if (curr != back_){
+      reverse_recursive_helper(curr->next, curr);
+    }else{
+      front_ = curr;
+    }
+    if (prev == nullptr){
+      back_ = curr;
+    }
+    curr->next = prev;
   }
 
 }  // namespace assignment
